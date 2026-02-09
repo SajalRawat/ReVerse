92d1387efa3000fc334726468c7ad5a6339b2ab6
SajalRawat <71058080+SajalRawat@users.noreply.github.com>
2026-02-09 23:05:28 +0530

    applied Animations to the flowers

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,41 @@ import Stalk from "/Stalk.svg"
 import FlowerComponents from './components/Flower'
 // import SunCursor from './components/SunCursor'
 import Line from './components/Line'
+import TrueFocus from './components/TrueFocus';
+import sunFlower from '/sunflowerSticker.svg'
 import './App.css'
+
 
 function App() {
 
   return (
     <div className='bg-black p-0 w-full min-h-screen overflow-y-auto relative flex flex-col items-center justify-start'>
-      
+
       <div className='flex items-center flex-col m-2'>
-        <p className='mainfont text-white text-[126px]'>ReVerse</p>
-        <p className='mainfont text-white text-[24px]'> Pune's Biggest <span className='bg-white text-black p-1'>Slam Poetry</span> Event</p>
+        <div className='flex '>
+          <img src={sunFlower} width={120} alt="" className='animation'/>
+          <p className='mainfont text-white text-[126px] px-6'>ReVerse</p>
+          <img src={sunFlower} width={80} className='animation' alt="" />
+
+        </div>
+        <p className='mainfont text-white text-[24px]'> <span className='text-yellow-300'>Pune's Biggest</span> <TrueFocus
+          sentence="Slam Poetry"
+          manualMode={false}
+          blurAmount={5}
+          borderColor="Yellow"
+          animationDuration={0.5}
+          pauseBetweenAnimations={1}
+        /> <span className='text-yellow-300'>Event</span></p>
       </div>
       {/* <SunCursor /> */}
       <div>
-      {/* <FlowerComponents /> */}
-      <img src={Stalk} alt="" className='' style={{ zIndex: 1 }} />
+        {/* <FlowerComponents /> */}
+        <img src={Stalk} alt="" className='' style={{ zIndex: 1 }} />
       </div>
-      <Line/>
+      <Line />
 
     </div>
-  )  
+  )
 }
 
 export default App
